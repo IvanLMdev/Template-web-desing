2ef0903a67ebc9f61b3b6ba99079b3297d94f740
IvanLMdev <ivanmantilla05@gmail.com>
2024-12-17 00:11:49 -0300

    commit-6

--- template-web-1/src/App.jsx
+++ template-web-1/src/App.jsx
@@ -1,8 +1,9 @@
-// import ImageGallery from "./components/ImageGallery"
+import ImageGallery from "./components/ImageGallery"
 import Header from "./components/Header"
 import ContFormAdress from "./components/ContFormAdress"
 import Footer from "./components/Footer"
 import InfoCard from "./components/InfoCard"
+
 import './styles/app.css'
 import {info} from './components/utilities/info.js'
 
@@ -12,7 +13,7 @@ function App() {
     <>
       <Header/>
       <InfoCard tittle={info[0].tittle} article={info[0].article} imgUrl={info[0].imgUrl}/>
-      {/* <ImageGallery/> */}
+      <ImageGallery/>
       <InfoCard tittle={info[1].tittle} article={info[1].article} imgUrl={info[1].imgUrl}/>
       <ContFormAdress/>
       <Footer/> 
--- template-web-1/src/components/ContFormAdress.jsx
+++ template-web-1/src/components/ContFormAdress.jsx
@@ -7,14 +7,14 @@ const ContFormAdress = () => {
     <section className="all-cont-form">
         <section className="cont-adress-link">
         <div className='adress'>
-            <h3>Locasiones</h3>
-            <p>ubicaciones </p>
+            <h3>Locaciones</h3>
+            <p>ubicacion</p>
             <address>calle falsa 1233 lomas falsas</address>
         </div>
         <div className="follow">
           <h4>Siguenos en redes</h4>
           <RedIcons />
-          <p>texto de pueva en icons red</p>
+          <p>texto de prueva en icons contact</p>
         </div>
       </section>
         <Form/>
