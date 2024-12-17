391fc10ba1383b725acac74a61ef5efdc3434025
IvanLMdev <ivanmantilla05@gmail.com>
2024-12-16 22:02:08 -0300

    segundo commit

--- template-web-1/src/components/utilities/info.js
+++ template-web-1/src/components/utilities/info.js
@@ -1,14 +1,16 @@
+import  photo from'../../../public/istockphoto.jpg'
+
 export const info = [
   {
     tittle: "Titulo De Articulo 1",
     article:
       " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis mollitia accusantium distinctio repudiandae debitis repellat fugiat dignissimos architecto, provident corrupti. Repudiandae dicta eum officiis saepe facilis nostrum molestiae distinctio unde natus deserunt tenetur debitis, odit dolorem aperiam perspiciatis id. Voluptatem consectetur error accusantium labore, deserunt non!",
-    imgUrl:"https://media.istockphoto.com/id/1222357475/vector/image-preview-icon-picture-placeholder-for-website-or-ui-ux-design-vector-illustration.jpg?s=612x612&w=0&k=20&c=KuCo-dRBYV7nz2gbk4J9w1WtTAgpTdznHu55W9FjimE="
+    imgUrl:{photo}
   },
   {
     tittle: "Titulo De Articulo 2",
     article:
       " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis mollitia accusantium distinctio repudiandae debitis repellat fugiat dignissimos architecto, provident corrupti. Repudiandae dicta eum officiis saepe facilis nostrum molestiae distinctio unde natus deserunt tenetur debitis, odit dolorem aperiam perspiciatis id. Voluptatem consectetur",
-      imgUrl:"https://media.istockphoto.com/id/1222357475/vector/image-preview-icon-picture-placeholder-for-website-or-ui-ux-design-vector-illustration.jpg?s=612x612&w=0&k=20&c=KuCo-dRBYV7nz2gbk4J9w1WtTAgpTdznHu55W9FjimE="
+      imgUrl:{photo}
   }
 ];
