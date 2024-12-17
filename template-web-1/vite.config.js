91d1a9e657a0d0090a15cde12240f5f37baecc48
IvanLMdev <ivanmantilla05@gmail.com>
2024-12-17 16:17:08 -0300

    add mayus

--- template-web-1/vite.config.js
+++ template-web-1/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://IvanLMdev.github.io/template-web-desing"
+  base:"https://IvanLMdev.github.io/Template-web-desing"
 })
